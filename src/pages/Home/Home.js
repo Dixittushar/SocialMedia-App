e19288944904ff175e557615434cda0669af336d
Dixittushar <dixittushar90@example.com>
2024-05-09 01:22:55 +0530

    Initial commit. Added Profile Card

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import ProfileSide from "../../components/ProfileSide/ProfileSide";
+const Home = () => {
+  return (
+    <div className="Home">
+      <ProfileSide />
+      <div className="PostSide">Posts</div>
+      <div className="RightSide">RightSide</div>
+    </div>
+  );
+};
+
+export default Home;
