3506ffdcb3812b4f7f640e4e60587a8bcc0c2ca4
Dixittushar <dixittushar90@example.com>
2024-05-10 12:12:06 +0530

    Added functionlities to display followers and post share card

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Home.css";
 import ProfileSide from "../../components/ProfileSide/ProfileSide";
+import PostSide from "../../components/PostSide/PostSide";
 const Home = () => {
   return (
     <div className="Home">
       <ProfileSide />
-      <div className="PostSide">Posts</div>
+      <PostSide />
       <div className="RightSide">RightSide</div>
     </div>
   );
